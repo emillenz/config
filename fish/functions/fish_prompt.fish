dc56465c47d57ac176cb201e67bf55be098291c2
Emil Lenz <emillenz@protonmail.com>
2024-05-05 13:25:26 +0200

    shell :: better prompt

--- fish/functions/fish_prompt.fish
+++ fish/functions/fish_prompt.fish
@@ -6,12 +6,13 @@
 #  ---
 
 function fish_prompt
-    set -l sep (string repeat --count (math (tput cols) - 2) "─")
+    set -l sep (set_color --background='#f2f2f2') (string repeat --count (math (tput cols) - 3) " ") (set_color normal) \n
     set -l stat
     if test $status -ne 0
-        set stat (set_color red)"[$status]"(set_color normal)
+        set stat (set_color red)"[$status]"
     end
-    set -l dir (set_color magenta) (prompt_pwd --full-length-dirs 4) (set_color normal)
+    set -l dir (set_color '#005f5f') (prompt_pwd --full-length-dirs 4)
+    set -l delimiter (set_color '#0031a9') ">"
 
-    echo \n (set_color --bold) \n $sep \n $dir $stat ":: " (set_color normal)
+    echo \n (set_color --background='#dae5ec' --bold) $dir $stat $delimiter (set_color normal) ""
 end

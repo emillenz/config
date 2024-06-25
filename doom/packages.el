f53fbf2f7043d701f73d7895bf8cbc3de62f91f3
lenz <emillenz@protonmail.com>
2024-06-25 14:19:20 +0200

    emacs :: replace snipe w. surround

--- doom/packages.el
+++ doom/packages.el
@@ -1,6 +1,9 @@
 ;; -*- no-byte-compile: t; -*-
 
-(disable-packages! evil-escape which-key solaire-mode) ;; bloat
+(disable-packages! evil-escape          ;; faster to remap <esc> to caps
+                   which-key            ;; bad for muscle memory & slow to lookup
+                   solaire-mode         ;; distracting visual sugar
+                   evil-snipe)          ;; faster to just use incsearch immeadiately (or f/t is enough)
 
 (package! dired-open)
 (package! rainbow-mode)
